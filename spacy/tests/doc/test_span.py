df08f4d17065343b65cbc919ea3238a99164c225
Daniël de Kok <me@danieldk.eu>
2022-01-12 13:38:52 +0100

    Span/SpanGroup: wrap SpanC in shared_ptr (#9869)
    * Span/SpanGroup: wrap SpanC in shared_ptr
    
    When a Span that was retrieved from a SpanGroup was modified, these
    changes were not reflected in the SpanGroup because the underlying
    SpanC struct was copied.
    
    This change applies the solution proposed by @nrodnova, to wrap SpanC in
    a shared_ptr. This makes a SpanGroup and Spans derived from it share the
    same SpanC. So, changes made through a Span are visible in the SpanGroup
    as well.
    
    Fixes #9556
    
    * Test that a SpanGroup is modified through its Spans
    
    * SpanGroup.push_back: remove nogil
    
    Modifying std::vector is not thread-safe.
    
    * C++ >= 11 does not allow const T in vector<T>
    
    * Add Span.span_c as a shorthand for Span.c.get
    
    Since this method is cdef'ed, it is only visible from Cython, so we
    avoid using raw pointers in Python
    
    Replace existing uses of span.c.get() to use this new method.
    
    * Fix formatting
    
    * Style fix: pointer types
    
    * SpanGroup.to_bytes: reduce number of shared_ptr::get calls
    
    * Mark SpanGroup modification test with issue
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- spacy/tests/doc/test_span.py
+++ spacy/tests/doc/test_span.py
@@ -5,7 +5,8 @@ from thinc.api import get_current_ops
 
 from spacy.attrs import LENGTH, ORTH
 from spacy.lang.en import English
-from spacy.tokens import Doc, Span, Token
+from spacy.tokens import Doc, Span, SpanGroup, Token
+from spacy.vocab import Vocab
 from spacy.util import filter_spans
 from spacy.vocab import Vocab
 
@@ -163,16 +164,16 @@ def test_char_span(doc, i_sent, i, j, text):
         assert span.text == text
 
 
-def test_char_span_attributes(doc):
-    label = "LABEL"
-    kb_id = "KB_ID"
-    span_id = "SPAN_ID"
-    span1 = doc.char_span(20, 45, label=label, kb_id=kb_id, span_id=span_id)
-    span2 = doc[1:].char_span(15, 40, label=label, kb_id=kb_id, span_id=span_id)
-    assert span1.text == span2.text
-    assert span1.label_ == span2.label_ == label
-    assert span1.kb_id_ == span2.kb_id_ == kb_id
-    assert span1.id_ == span2.id_ == span_id
+@pytest.mark.issue(9556)
+def test_modify_span_group(doc):
+    group = SpanGroup(doc, spans=doc.ents)
+    for span in group:
+        span.start = 0
+        span.label = doc.vocab.strings["TEST"]
+
+    # Span changes must be reflected in the span group
+    assert group[0].start == 0
+    assert group[0].label == doc.vocab.strings["TEST"]
 
 
 def test_spans_sent_spans(doc):
