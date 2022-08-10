cfc21ce3a5e6f67a91bc9145d893736b504147a7
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2022-08-10 11:44:05 +0200

    Rename modules for consistency (#11286)
    * rename Python module to entity_ruler
    
    * rename Python module to attribute_ruler

--- spacy/pipeline/__init__.py
+++ spacy/pipeline/__init__.py
@@ -1,9 +1,9 @@
-from .attributeruler import AttributeRuler
+from .attribute_ruler import AttributeRuler
 from .dep_parser import DependencyParser
 from .edit_tree_lemmatizer import EditTreeLemmatizer
 from .entity_linker import EntityLinker
-from .entityruler import EntityRuler
-from .functions import merge_entities, merge_noun_chunks, merge_subtokens
+from .ner import EntityRecognizer
+from .entity_ruler import EntityRuler
 from .lemmatizer import Lemmatizer
 from .morphologizer import Morphologizer
 from .ner import EntityRecognizer
