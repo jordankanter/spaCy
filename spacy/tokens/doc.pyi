d4703ea17bd21e1dafe093e7e1eb289548cf4f06
Adriane Boyd <adrianeboyd@gmail.com>
2022-08-30 13:56:35 +0200

    Make stable private modules public and adjust names (#11353)
    * Make stable private modules public and adjust names
    
    * `spacy.ml._character_embed` -> `spacy.ml.character_embed`
    * `spacy.ml._precomputable_affine` -> `spacy.ml.precomputable_affine`
    * `spacy.tokens._serialize` -> `spacy.tokens.doc_bin`
    * `spacy.tokens._retokenize` -> `spacy.tokens.retokenize`
    * `spacy.tokens._dict_proxies` -> `spacy.tokens.span_groups`
    
    * Skip _precomputable_affine
    
    * retokenize -> retokenizer
    
    * Fix imports

--- spacy/tokens/doc.pyi
+++ spacy/tokens/doc.pyi
@@ -17,7 +17,10 @@ from typing import (
 import numpy as np
 from cymem.cymem import Pool
 from thinc.types import Floats1d, Floats2d, Ints2d
-
+from .span import Span
+from .token import Token
+from .span_groups import SpanGroups
+from .retokenizer import Retokenizer
 from ..lexeme import Lexeme
 from ..vocab import Vocab
 from ._dict_proxies import SpanGroups
