b8dd79a0e6b36784a00e9015df65c3ba94cc48d1
Paul O'Leary McCann <polm@dampfkraft.com>
2022-12-08 19:45:52 +0900

    Remove old model shortcuts (#11916)
    * Remove old model shortcuts
    
    * Remove error, docs warnings about shortcuts
    
    * Fix import in util
    
    Accidentally deleted the whole import and not just the old part...
    
    * Change universe example to v3 style
    
    * Switch ubuntu-latest to ubuntu-20.04 in main tests (#11928)
    
    * Switch ubuntu-latest to ubuntu-20.04 in main tests
    
    * Only use 20.04 for 3.6
    
    * Update some model loading in Universe
    
    * Add v2 tag to neuralcoref
    
    * Use the spacy-version feature instead of a v2 tag
    
    Co-authored-by: svlandeg <svlandeg@github.com>

--- spacy/cli/download.py
+++ spacy/cli/download.py
@@ -7,16 +7,8 @@ import typer
 from wasabi import msg
 
 from .. import about
-from ..errors import OLD_MODEL_SHORTCUTS
-from ..util import (
-    get_minor_version,
-    is_in_interactive,
-    is_in_jupyter,
-    is_package,
-    is_prerelease_version,
-    run_command,
-)
-from ._util import SDIST_SUFFIX, WHEEL_SUFFIX, Arg, Opt, app
+from ..util import is_package, get_minor_version, run_command
+from ..util import is_prerelease_version
 
 
 @app.command(
