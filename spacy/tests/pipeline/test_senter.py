500711c8ace06d20db9a333e2289ebdc5bbefa78
Daniël de Kok <me@danieldk.eu>
2023-01-16 10:25:53 +0100

    Add `TrainablePipe.{distill,get_teacher_student_loss}` (#12016)
    * Add `TrainablePipe.{distill,get_teacher_student_loss}`
    
    This change adds two methods:
    
    - `TrainablePipe::distill` which performs a training step of a
       student pipe on a teacher pipe, giving a batch of `Doc`s.
    - `TrainablePipe::get_teacher_student_loss` computes the loss
      of a student relative to the teacher.
    
    The `distill` or `get_teacher_student_loss` methods are also implemented
    in the tagger, edit tree lemmatizer, and parser pipes, to enable
    distillation in those pipes and as an example for other pipes.
    
    * Fix stray `Beam` import
    
    * Fix incorrect import
    
    * Apply suggestions from code review
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>
    
    * TrainablePipe.distill: use `Iterable[Example]`
    
    * Add Pipe.is_distillable method
    
    * Add `validate_distillation_examples`
    
    This first calls `validate_examples` and then checks that the
    student/teacher tokens are the same.
    
    * Update distill documentation
    
    * Add distill documentation for all pipes that support distillation
    
    * Fix incorrect identifier
    
    * Apply suggestions from code review
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>
    
    * Add comment to explain `is_distillable`
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- spacy/tests/pipeline/test_senter.py
+++ spacy/tests/pipeline/test_senter.py
@@ -11,6 +11,12 @@ from spacy.tests.util import make_tempdir
 from spacy.training import Example
 
 
+def test_is_distillable():
+    nlp = English()
+    senter = nlp.add_pipe("senter")
+    assert senter.is_distillable
+
+
 def test_label_types():
     nlp = Language()
     senter = nlp.add_pipe("senter")
