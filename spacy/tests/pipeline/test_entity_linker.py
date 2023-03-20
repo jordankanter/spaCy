1a7fe5f28222ab4489db79dcb43fefa521d92989
Raphael Mitsch <r.mitsch@outlook.com>
2023-03-20 12:25:18 +0100

    Entity linking: use `SpanGroup` instead of `Iterable[Span]` for mentions (#12344)
    * Convert Candidate from Cython to Python class.
    
    * Format.
    
    * Fix .entity_ typo in _add_activations() usage.
    
    * Change type for mentions to look up entity candidates for to SpanGroup from Iterable[Span].
    
    * Update docs.
    
    * Update spacy/kb/candidate.py
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>
    
    * Update doc string of BaseCandidate.__init__().
    
    * Update spacy/kb/candidate.py
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>
    
    * Rename Candidate to InMemoryCandidate, BaseCandidate to Candidate.
    
    * Adjust Candidate to support and mandate numerical entity IDs.
    
    * Format.
    
    * Fix docstring and docs.
    
    * Update website/docs/api/kb.mdx
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>
    
    * Rename alias -> mention.
    
    * Refactor Candidate attribute names. Update docs and tests accordingly.
    
    * Refacor Candidate attributes and their usage.
    
    * Format.
    
    * Fix mypy error.
    
    * Update error code in line with v4 convention.
    
    * Reverse erroneous changes during merge.
    
    * Update return type in EL tests.
    
    * Re-add Candidate to setup.py.
    
    * Format updated docs.
    
    ---------
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- spacy/tests/pipeline/test_entity_linker.py
+++ spacy/tests/pipeline/test_entity_linker.py
@@ -1090,7 +1090,6 @@ def test_scorer_links():
 )
 # fmt: on
 def test_legacy_architectures(name, config):
-
     # Ensure that the legacy architectures still work
     vector_length = 3
     nlp = English()
