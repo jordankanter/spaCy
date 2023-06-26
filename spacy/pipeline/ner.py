2b4fa1f2970cf998c51eed44a30721f01e5a96d6
Daniël de Kok <me@danieldk.eu>
2023-06-26 11:41:03 +0200

    isort all the things

--- spacy/pipeline/ner.py
+++ spacy/pipeline/ner.py
@@ -11,20 +11,6 @@ from ..util import registry
 from ._parser_internals.ner import BiluoPushDown
 from ._parser_internals.transition_system import TransitionSystem
 from .transition_parser import Parser
-from ._parser_internals.ner import BiluoPushDown
-from ..language import Language
-from ..scorer import get_ner_prf, PRFScore
-from ..training import validate_examples
-from ..util import registry
-from ..training import remove_bilu_prefix
-
-from ._parser_internals.ner cimport BiluoPushDown
-from .transition_parser cimport Parser
-
-from ..language import Language
-from ..scorer import get_ner_prf
-from ..training import remove_bilu_prefix
-from ..util import registry
 
 default_model_config = """
 [model]
