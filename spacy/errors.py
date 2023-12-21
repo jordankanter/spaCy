a96dfbf8d34ceb432c993fb1df63e6027059b1e5
Daniël de Kok <me@danieldk.eu>
2023-12-21 11:00:06 +0100

    Add TextCatReduce.v1 (#13181)
    * Add TextCatReduce.v1
    
    This is a textcat classifier that pools the vectors generated by a
    tok2vec implementation and then applies a classifier to the pooled
    representation. Three reductions are supported for pooling: first, max,
    and mean. When multiple reductions are enabled, the reductions are
    concatenated before providing them to the classification layer.
    
    This model is a generalization of the TextCatCNN model, which only
    supports mean reductions and is a bit of a misnomer, because it can also
    be used with transformers. This change also reimplements TextCatCNN.v2
    using the new TextCatReduce.v1 layer.
    
    * Doc fixes
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>
    
    * Fully specify `TextCatCNN` <-> `TextCatReduce` equivalence
    
    * Move TextCatCNN docs to legacy, in prep for moving to spacy-legacy
    
    * Add back a test for TextCatCNN.v2
    
    * Replace TextCatCNN in pipe configurations and templates
    
    * Add an infobox to the `TextCatReduce` section with an `TextCatCNN` anchor
    
    * Add last reduction (`use_reduce_last`)
    
    * Remove non-working TextCatCNN Netlify redirect
    
    * Revert layer changes for the quickstart
    
    * Revert one more quickstart change
    
    * Remove unused import
    
    * Fix docstring
    
    * Fix setting name in error message
    
    ---------
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>
    Co-authored-by: Adriane Boyd <adrianeboyd@gmail.com>

--- spacy/errors.py
+++ spacy/errors.py
@@ -976,6 +976,9 @@ class Errors(metaclass=ErrorsWithCodes):
     E1055 = ("The 'replace_listener' callback expects {num_params} parameters, "
              "but only callbacks with one or three parameters are supported")
     E1056 = ("The `TextCatBOW` architecture expects a length of at least 1, was {length}.")
+    E1057 = ("The `TextCatReduce` architecture must be used with at least one "
+             "reduction. Please enable one of `use_reduce_first`, "
+             "`use_reduce_last`, `use_reduce_max` or `use_reduce_mean`.")
 
     # v4 error strings
     E4000 = ("Expected a Doc as input, but got: '{type}'")
