d6dcab9c4bb444c367d189cd688e3bba3c0f657c
Daniël de Kok <me@danieldk.eu>
2024-01-16 14:54:26 +0100

    Update thinc dependency to 9.0.0.dev4

--- spacy/pipeline/senter.pyx
+++ spacy/pipeline/senter.pyx
@@ -2,10 +2,7 @@
 from itertools import islice
 from typing import Callable, Dict, Iterable, List, Optional, Union
 
-import srsly
-from thinc.api import Config, Model
-from thinc.legacy import LegacySequenceCategoricalCrossentropy
-from thinc.types import Floats2d, Ints1d
+from thinc.api import Config, Model, SequenceCategoricalCrossentropy
 
 from ..tokens.doc cimport Doc
 
@@ -183,7 +180,7 @@ class SentenceRecognizer(Tagger):
         """
         validate_examples(examples, "SentenceRecognizer.get_loss")
         labels = self.labels
-        loss_func = LegacySequenceCategoricalCrossentropy(names=labels, normalize=False)
+        loss_func = SequenceCategoricalCrossentropy(names=labels, normalize=False)
         truths = []
         for eg in examples:
             eg_truth = []
